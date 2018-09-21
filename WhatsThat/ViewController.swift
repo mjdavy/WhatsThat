89885d048d44be94d8f1847c91ad4be3c67a9cd3
Martin Davy <mjdavy@hotmail.com>
2018-09-21 11:21:02 -0400

    Initial commit with complete UI layout

--- WhatsThat/ViewController.swift
+++ WhatsThat/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classifier: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func camera(_ sender:Any) {
+        
+        if (!UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            return
+        }
+        
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker, animated: true)
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
     }
 
 
